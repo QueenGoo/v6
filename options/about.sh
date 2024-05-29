27eaf7d1660a1f774c0eb37cc3db485ac21dfe49
QueenGoo <muhfaruq2024@gmail.com>
2024-05-30 00:19:37 +0700

    Add files via upload

---
+++ options/about.sh
@@ -0,0 +1,18 @@
+echo -e "┌─────────────────────────────────────────────────┐" | lolcat
+echo -e "│               AUTOSCRIPT PREMIUM                │" | lolcat
+echo -e "│             BY QUEEN STORE             │" | lolcat
+echo -e "└─────────────────────────────────────────────────┘" | lolcat
+
+echo -e "> Support OS 
+  For Ubuntu 18.04 & Ubuntu 20.04 & Ubuntu 22.04 
+  For Debian 9 & Debian 10 & Debian 11 " | lolcat
+echo ""
+echo -e "  Contact Owner AutoScript
+> WA: 6283857026355
+> TELEGRAM: t.me/Queen_Storeb" | lolcat
+echo ""
+echo -e "Thanks for your Order Queen Store" | lolcat
+echo ""
+read -n 1 -s -r -p "  Press any key to Back to Menu"
+menu
+fi
